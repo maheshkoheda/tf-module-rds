d0bf44d3695b081de1c2bba63aaf7b19e79aa751
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-10-16 02:58:44 -0400

    VPC Subnets routetables and associateion,gateways and nat gateways through modules concept and automated terraform with Docdb and load balancer+ Aurora DB

---
+++ vars.tf
@@ -0,0 +1,8 @@
+variable "env" {}
+variable "tags" {}
+#subnet_ids = local.db_subnets
+#vpc_id = local.vpc_id
+#sg_ingress_cidr = local.app_subnets_cidr
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "sg_ingress_cidr" {}
